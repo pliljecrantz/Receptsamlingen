58ce284650ee9ef796d444dd50f9e6e1529a9477
Petra Liljecrantz <p.liljecrantz@gmail.com>
2019-05-01 20:19:14 +0200

    Attempt 2 to fix logging

--- Receptsamlingen.Mvc/Properties/AssemblyInfo.cs
+++ Receptsamlingen.Mvc/Properties/AssemblyInfo.cs
@@ -20,15 +20,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0e5814ff-b997-4298-9966-4f978428acc6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
